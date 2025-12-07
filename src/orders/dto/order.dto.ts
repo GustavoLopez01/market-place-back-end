998c8d4fd22d22c6133f9afd2b3e0959576028ff
Gustavo <gustavoalzarate@gmail.com>
2025-12-07 16:09:45 -0600

    feat: add validation to order dto

--- src/orders/dto/order.dto.ts
+++ src/orders/dto/order.dto.ts
@@ -1,12 +1,22 @@
+import { IsArray, IsNumber } from 'class-validator';
 import { OrderProducts } from '../orderProduct.entity';
 
 export class CreateOrder {
-
+  @IsNumber({}, {
+    message: 'La cantidad de productos debe ser un número'
+  })
   quantityProducts: number;
 
+  @IsNumber({}, {
+    message: 'La cantidad de productos debe ser un número'
+  })
   total: number;
 
+  @IsNumber({}, {
+    message: 'La cantidad de productos debe ser un número'
+  })
   userId: number
 
+  @IsArray({})
   products: OrderProducts[];
 }
